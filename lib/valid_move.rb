4beb374757220a16e44de51470bdee28da8a2192
Grace Berman <grace.g.berman@gmail.com>
2020-01-05 19:42:07 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -6,7 +6,7 @@ def valid_move?(board, index)
       return true
     end
   end
-  if index.between?(0, 8) && position_taken? == false
+  if index.between?(0, 8) && position_taken?(board, index) == false
     return true
   else return false
   end
